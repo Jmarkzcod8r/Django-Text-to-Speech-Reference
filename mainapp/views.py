5b01e553234157ff96e9da7d3cea96e5c3ed925a
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2023-01-20 00:29:30 +0800

    First for pythonanywhere

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime
 from django.shortcuts import render, redirect, HttpResponse
 from gtts import gTTS
 import string
@@ -9,36 +10,33 @@ import shutil
 
 def index_page(request):
     print("hi")
+    
     if request.method == "POST":
+        # RedGUI()
+        now = datetime.now()
+        dt_string = now.strftime("%d-%m-%Y_%H:%M:%S")
         letters = string.ascii_lowercase
 
-        file_name = f"{''.join(random.choice(letters) for i in range(10))}.mp3"
+        file_name = f"{dt_string}{''.join(random.choice(letters) for i in range(10))}.mp3"
 
         text = request.POST['text']
         tdl = request.POST['tdl']
         lang = request.POST['lang']
 
         tts = gTTS(text, lang=lang, tld=tdl)
+        # tts = gTTS(text, lang=lang, tld='com.au')
+        print(file_name)
         tts.save(file_name)
+        tts.save('okay.mp3')
 
+        os.system("mpg321 okay.mp3")
         dir = os.getcwd()
         full_dir = os.path.join(dir, file_name)
-        print(dir)
-        print(full_dir)
-
-        dest = shutil.move(full_dir, os.path.join(
-            dir, "mainapp\static\sound_file"))
+        dest = shutil.move(full_dir, os.path.join(dir, "mainapp/static/sound_file"))
 
         data = {"loc" :file_name}
         return render(request,'download.html',data)
-
-    return render(request, 'index.html')
-
-
-# from gtts import gTTS
-# tts_en = gTTS('hello', lang='en')
-# tts_fr = gTTS('bonjour', lang='fr')
-# >>>
-# with open('hello_bonjour.mp3', 'wb') as f:
-#     tts_en.write_to_fp(f)
-#     tts_fr.write_to_fp(f)
+    
+        # return render(request, 'index.html', {'loc':text})
+    return render(request, 'index.html', {'loc':''})
+    # return None
